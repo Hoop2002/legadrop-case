e0bb880fc3fc6456d056eb2c9ad06c56f702b415
ushakek <ushakek@yandex.ru>
2024-01-23 22:14:46 +0500

    reformat with black

--- routers/admin/promocodes/functions.py
+++ routers/admin/promocodes/functions.py
@@ -5,9 +5,16 @@ from database import get_session
 from models import PromoCode
 
 
-async def get_promo(filter_by: dict = {}, page_size: int = 20, page: int = 0) -> Sequence[PromoCode]:
+async def get_promo(
+    filter_by: dict = {}, page_size: int = 20, page: int = 0
+) -> Sequence[PromoCode]:
     async with get_session() as session:
-        stmt = select(PromoCode).filter_by(**filter_by).limit(page_size).offset(page * page_size)
+        stmt = (
+            select(PromoCode)
+            .filter_by(**filter_by)
+            .limit(page_size)
+            .offset(page * page_size)
+        )
         result = await session.execute(stmt)
         promo = result.scalars().all()
         return promo
--- routers/admin/promocodes/promocodes.py
+++ routers/admin/promocodes/promocodes.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
 from security import verify_admin
 from models import RequestAdminID, PromoCode
-from models.promo_schemas import PromoCodeRequestSchema, ListPromoCodesSchema, PromoCodeResponseSchema
+from models.promo_schemas import (
+    PromoCodeRequestSchema,
+    ListPromoCodesSchema,
+    PromoCodeResponseSchema,
+)
 from .functions import get_promo
 
 
@@ -10,7 +14,9 @@ router = APIRouter()
 
 
 @router.get("/promo", response_model=ListPromoCodesSchema)
-async def get_promo_codes(page: int = 0, page_size: int = 20, admin: RequestAdminID = Depends(verify_admin)):
+async def get_promo_codes(
+    page: int = 0, page_size: int = 20, admin: RequestAdminID = Depends(verify_admin)
+):
     promo_codes = await get_promo(page_size=page_size, page=page)
     return promo_codes
 
@@ -20,13 +26,18 @@ async def get_promo_code(promo_id: int, admin: RequestAdminID = Depends(verify_a
     promo_code = await get_promo(filter_by=dict(id=promo_id))
     promo_code = promo_code[0]
     if not promo_code:
-        return JSONResponse({
-            "message": "Такой промокод не найден"
-        }, status_code=status.HTTP_400_BAD_REQUEST)
+        return JSONResponse(
+            {"message": "Такой промокод не найден"},
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
     return promo_code
 
 
-@router.post("/promo/create_promo", response_model=PromoCodeResponseSchema, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/promo/create_promo",
+    response_model=PromoCodeResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+)
 async def create_promo_code(
     promo_data: PromoCodeRequestSchema, admin: RequestAdminID = Depends(verify_admin)
 ):
